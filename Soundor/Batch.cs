f7c80f0b8b50fb2c2332eaf0637d66b58e803256
Marc <RhinoDevel@users.noreply.github.com>
2026-09-01 17:31:05 +0200

    "Infrastructure" for batch mode (to be implemented).

---
+++ Soundor/Batch.cs
@@ -0,0 +1,13 @@
+﻿
+// 2026sep01, Marcel Timm, RhinoDevel
+
+namespace Soundor
+{
+    internal static class Batch
+    {
+        internal static bool Exec(string fileName)
+        {
+            return false; // TODO: Implement!
+        }
+    }
+}
